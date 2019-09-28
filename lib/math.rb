83d8dd7e70f0de0a1e4f2a6ced3294cd0dfb8e7f
Tavis Weeden <tavisweeden@gmail.com>
2019-09-28 20:23:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,18 +1,23 @@
 def addition(num1, num2)
   num1 + num2
+
 end
 
 addition(5, 4)
 
 def subtraction(num1, num2)
   num1 - num2
+
 end
 
 subtraction(10, 5)
 
 def division(num1, num2)
+  num1 / num2
 
 end
+
+division(50, 2)
 
 def multiplication(num1, num2)
 
